7461ceec000b7d94ed20d47867c67764b1325720
alu01007134351 <noelpadron1415@gmail.com>
2015-11-10 19:17:38 +0000

    Preguntas

--- models/quiz_model.js
+++ models/quiz_model.js
@@ -1,64 +1,76 @@
-var AbstractQuiz = require('models/abstract_quiz_model.js');
+var AbstractQuiz = require('../models/abstract_quiz_model.js');
 
 function Quiz() {
+
   AbstractQuiz.call(this);
+
   this.q.push(
-    { pregunta: '¿Capital de Italia?',
+
+    { pregunta: '¿Cuantos anillos ha ganado Michael Jordan?',
       respuesta: function(x) {
-        return (/^\s*roma\s*$/i).exec(x);
+        return (/^\s*seis\s*$/i).exec(x);
       }
     },
+
     {
-      pregunta: '¿Quien reinaba en España cuando se descubrió América?',
+      pregunta: '¿Quien es el entrenador con más anillos de la NBA?',
       respuesta: function(x) {
-        if ((/\b(Isabel\s+y?\s*Fernando)|(Fernando\s+[ey]?\s*Isabel)\b/i).exec(x)) {
-          return true;
-        }
-        if ((/\breyes\s+cat[oó]licos\b/i).exec(x)) { return true; }
-        return false;
-      },
+        return (/\s*phill jackson\s*$/i).exec(x);
+      }
     },
-    { /* Código inseguro. ¡No ejecute esta pregunta salvo en un
-         entorno en el que el código del "alumno" sea fiable!
-       */
-      pregunta: 'Escriba una función JavaScript de nombre <tt>square</tt> '+
-      'que recibe un número y devuelve el cuadrado',
+
+    { pregunta: '¿Cuantos titulos ganó el Real Madrid Baloncesto en la pasada temporada?',
       respuesta: function(x) {
-        try {
-          eval(x); /* DANGER DANGER DANGER */
-          var z = Math.floor(Math.random()*100);
-          return (square(z) == z*z);
-        }
-        catch(e) {
-          return false;
-        }
-        return false;
+        return (/\s*cinco\s*$/i).exec(x);
+      }
+    },
+
+    { pregunta: '¿Cúal es la nacionalidad de Milos Teodosic?',
+      respuesta: function(x) {
+        return (/\s*serbio\s*$/i).exec(x);
+      }
+    },
+    
+    { pregunta: '¿En qué año murió Drazen Petrovic?',
+      respuesta: function(x) {
+        return (/\s*1993\s*$/i).exec(x);
+      }
+    },
+    
+    { pregunta: '¿Quien fue el mejor amigo de Petrovic en sus comienzos en la NBA?',
+      respuesta: function(x) {
+        return (/\s*vlade divac\s*$/i).exec(x);
       }
     }
+
   );
+
   // insertar unas cuantas preguntas sobre
   // la tabla de multiplicar
   var self  = this;
   for(var i = 0; i<3;i++) {
     (function() {
       var n1 = Math.randomInt(9)+1;
-      debug("n1 = "+n1);
       var n2 = Math.randomInt(9)+1;
-      debug("n2 = "+n2);
       self.q.push(
-        { pregunta: '¿ '+n1+'x'+n2+"= ?",
+        { pregunta: '¿ '+n1+'x'+n2+" ?",
           respuesta: function(x) {
-            debug("n1 = "+n1);
-            debug("n2 = "+n2);
             return (x == n1*n2);
         }
       })
     })();
   }
-  debug(this.q);
 }
 
 Quiz.prototype = new AbstractQuiz();
 Quiz.prototype.constructor = Quiz;
+
+Quiz.prototype.numQuestions = function() {
+  return this.q.length;
+}
+
+Quiz.prototype.getQ = function(x){
+  return this.q[x]['pregunta'];
+}
 
 module.exports = Quiz;
