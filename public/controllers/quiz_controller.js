1966fa8bb2b533ac73793e1701ac6c337ad2bc5f
alu01007134351 <noelpadron1415@gmail.com>
2015-11-10 18:45:53 +0000

    Probando mas

--- public/controllers/quiz_controller.js
+++ public/controllers/quiz_controller.js
@@ -1,4 +1,4 @@
-//GET question
+/*//GET question
 exports.question = function(req,res) {
   res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
@@ -11,4 +11,48 @@ exports.answer = function(req, res) {
     else{
         res.render('quizes/answer', {respuesta: 'Incorrecto'})
     }
+};*/
+var Quiz = require('../models/quiz_model');
+
+var quiz = new Quiz();
+var current = quiz.randomQuestion();
+
+exports.question = function(req,res) {
+  current = quiz.randomQuestion();
+  res.render('quizes/question', {pregunta: current.pregunta});
+};
+
+exports.answer = function(req, res) {
+  var c = 'Incorrecto';
+  if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
+  res.render('quizes/answer', {respuesta: c})
+};
+
+exports.questions = function(req,res) {
+  var nPreg = quiz.numQuestions();
+  var cadena = "";
+
+  for(var i=0; i<nPreg; i++) {
+    cadena = cadena.concat("Pregunta " + (i+1) + ": " + quiz.getQ(i) + "<br><br>");
+  }
+
+  res.render('quizes/questions', {prg: cadena})
+};
+
+exports.specificQuestion = function(req, res) {
+  var id = req.params.id;
+  var nPreg = quiz.numQuestions();
+
+  if(id < 1 || id > nPreg){
+    res.render('quizes/SpecificQuestion', {prg: "No existe esa pregunta."})
+  }
+  else if(isNaN(id) === true) {
+    res.render('quizes/SpecificQuestion', {prg: "Error en la URL."})
+  }
+  else {
+    id = id - 1;
+    var cadena = "Pregunta " + (id+1) + ": " + quiz.getQ(id);
+
+    res.render('quizes/SpecificQuestion', {prg: cadena})
+  }
 };
