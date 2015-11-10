1f2658709095ca1cf1f639dbed38fc622c89e540
alu01007134351 <noelpadron1415@gmail.com>
2015-11-10 18:35:13 +0000

    Empezado con las preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Quiz = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/preguntas', function(req, res);
+
+router.get('/', function(req, res);
 
 module.exports = router;
