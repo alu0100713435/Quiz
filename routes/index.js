f9f0fdc60c4d09e3e373f10e5153560e6cc1c138
alu01007134351 <noelpadron1415@gmail.com>
2015-11-10 19:04:47 +0000

    Arrancando primera prueba

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 
-router.get('/', quizController.index);
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+//router.get('/quizes/questions', quizController.questions);
+
+//router.get('/quizes/questions/:id', quizController.specificQuestion);
 
 module.exports = router;
