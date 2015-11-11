84a03b395eb76aa69ffb010670bca093a543ae0c
alu01007134351 <noelpadron1415@gmail.com>
2015-11-11 21:40:40 +0000

    Problemas con rutas

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Gran Quiz' });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
-router.get('/quizes/question_list', quizController.questions);
+router.get('/quizes/questions', quizController.questions);
 
-router.get('/quizes/question_list/:id', quizController.specificQuestion);
+router.get('/quizes/questions/:id', quizController.specificQuestion);
 
 module.exports = router;
