04e4e2f57824d06ae2d04f656ebb9cb261701f3a
alu01007134351 <noelpadron1415@gmail.com>
2015-11-11 23:54:45 +0000

    Express-helper

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var routes = require('./routes/index');
 //var users = require('./routes/users');
 
 var app = express();
+
 var helpers = require('express-helpers')(app);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
