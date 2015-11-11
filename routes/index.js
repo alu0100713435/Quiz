d5b84f00ebae95511db64a8d3b9b1115af580a6f
alu01007134351 <noelpadron1415@gmail.com>
2015-11-11 23:46:32 +0000

    Añadiendo listado de preguntas

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ router.get('/', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
-router.get('/quizes/questions', quizController.questions);
+router.get('/quizes/question_list', quizController.questions);
 
-router.get('/quizes/questions/:id', quizController.specificQuestion);
+router.get('/quizes/question_list/:id', quizController.specificQuestion);
 
 module.exports = router;
